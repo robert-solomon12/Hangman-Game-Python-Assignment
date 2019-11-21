a10740316f06083c19bba5c7afb2f37d528b3dec
Robert Solomon <20079462@mail.wit.ie>
2019-11-21 17:07:00 +0000

    ADDED CODE FOR HANGMAN GAME
    hangman game code

---
+++ src/Hangman-Game.py
@@ -0,0 +1,67 @@
+#
+#
+
+import random
+
+carbrands = ["ford","nissan","mustang","mercedes","porsche","bmw","fiat","volkswagen","audi","lamboghini","golf"]
+
+random.shuffle(carbrands)
+
+ans = list(carbrands[0])
+
+
+#print (ans)
+
+
+#empty list called show
+
+show = []
+
+# adds the variable answer to show
+show.extend(ans)
+
+
+#print(used)
+
+#iterates through the list show
+
+for i in range (len(show)):
+    #replaces each index in the list with "_"
+    show[i] = "_"
+
+#the join command puts a space between each '_'
+print (' '.join(show))
+print()
+
+#counter stops the game once all the letters have been guessed correctly
+counter = 0
+attempts = 5
+#while loop to keep asking user until all letters have been guessed correctly
+
+while counter < len(ans):
+    guess = input("Please guess the word by guessing a letter: ")
+    guess = guess.lower()
+    print (counter)
+
+
+    #iterates through the letters in the answer per index in the list
+    for i in range(len(ans)):
+        #if the guessed letter matches a letter in the answer ##then replace
+        if ans[i] == guess :
+    #then replace the index of that guessed word with the absolute guessed word
+          show[i] = guess
+          counter = counter + 1
+          print("correct, keep guessing, you have ! ")
+
+   #if the guessed word does not equal the answer then decrement attempts 
+        if ans[i] not guess :
+          attempts = attempts - 1
+          print("wrong!, you have" + attempts + "left!")
+
+#          print (used)
+
+    #print out the new string with guessed letters in
+    print (' '.join(show))
+#    print()
+
+print("Congratulations!, you've guessed the Word correctly")
